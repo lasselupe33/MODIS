f940a5fbba0e480f6c18dad84ca9c9c3d65049d9
Lasse Agersten <lasse@felskov-aps.dk>
2018-08-27 15:15:46 +0200

    Added client-code to project

---
+++ handin1/UDPClient.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner; 
+
+public class UDPClient{
+	private static int serverPort = 7007; 
+
+
+    public static void main(String args[]){ 
+		DatagramSocket aSocket = null;
+		Scanner msgScan = new Scanner(System.in);
+
+		while(true) { //Keep ask user for messages. 
+			try {
+			aSocket = new DatagramSocket(); 
+			InetAddress aHost = InetAddress.getByName("localhost");
+			String msg = msgScan.nextLine();
+			byte [] m = msg.getBytes();		                                                 
+			DatagramPacket request =
+				new DatagramPacket(m,  msg.length(), aHost, serverPort);
+			aSocket.send(request);			                        
+			byte[] buffer = new byte[1000];
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);	
+			aSocket.receive(reply);
+			System.out.println("Reply: " + new String(reply.getData()));
+			}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+			}catch (IOException e){System.out.println("IO: " + e.getMessage());
+			}finally {if(aSocket != null) aSocket.close();}
+		}
+	}	      	
+}
--- handin1/src/main.java
+++ handin1/src/main.java
@@ -1,4 +1,6 @@
 public class main {
+    private static int serverPort = 7007;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
