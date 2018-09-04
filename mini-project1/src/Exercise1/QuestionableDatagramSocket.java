e72d7502a4cdeca1df250489267de030b792b72e
Sif Kristensen <psiphph@Sifs-MacBook-Pro.local>
2018-09-04 12:57:31 +0200

    Created package for exercise 1

---
+++ mini-project1/src/Exercise1/QuestionableDatagramSocket.java
@@ -0,0 +1,9 @@
+package Exercise1;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class QuestionableDatagramSocket extends DatagramSocket {
+    public QuestionableDatagramSocket() throws SocketException {
+    }
+}
