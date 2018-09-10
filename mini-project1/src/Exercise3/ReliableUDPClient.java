0c04284c1ba35029beb6a216fb0f9cb8f2ba5058
lage <lage@itu.dk>
2018-09-10 14:15:12 +0200

    Added notes for exercise 3

--- mini-project1/src/Exercise3/ReliableUDPClient.java
+++ mini-project1/src/Exercise3/ReliableUDPClient.java
@@ -1,5 +1,21 @@
 package Exercise3;
 
+import java.util.Scanner;
+
 public class ReliableUDPClient {
-    // N
+    public static void main(String[] args) {
+        String msg = "Hardcoded string";
+
+        send(msg, "localhost", 7020);
+    }
+
+    public static void send(String string, String destIp, int destPort) {
+        // Send initial handshake to server
+
+
+        // Wait for response from server and verify
+
+
+        // Send msg to server
+    }
 }
--- mini-project1/src/Exercise3/ReliableUDPServer.java
+++ mini-project1/src/Exercise3/ReliableUDPServer.java
@@ -1,8 +1,17 @@
 package Exercise3;
 
 public class ReliableUDPServer {
+    public static void main(String[] args) {
+        // Wait for handshake
+
+        // Send acknowledgement + handshake back
+
+        // Wait for message, verify overhead information
+    }
+
+
+
     // NOTES:
-    // Hashing
 
     // Client-to-server handshake seq x
     // Server-to-client handshake ack seq x+1, seq y
